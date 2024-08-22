51dcb4a0ffd3e28778bd774bc2cac7f961d82c6c
Nicolas-Melluso <nicolas.e.melluso@gmail.com>
2024-08-21 23:18:30 -0300

    popup added for validation, information table saved in localstorage in JSON

--- public/blackjack/dealer/JS/blackjack-config.js
+++ public/blackjack/dealer/JS/blackjack-config.js
@@ -1,5 +1,10 @@
+const title = document.getElementById('title-select');
+const time = document.getElementById('time-select');
 const minBetSelect = document.getElementById('min-bet-select');
 const maxBetSelect = document.getElementById('max-bet-select');
+const pokerRounds = document.getElementById('rounds-poker');
+const cheats = document.getElementById('cheated');
+
 
 function updateMaxBetOptions() {
     const minValue = parseInt(minBetSelect.value);
@@ -18,3 +23,33 @@ function updateMaxBetOptions() {
 
 minBetSelect.addEventListener('change', updateMaxBetOptions);
 
+const readyButton = document.getElementById('send-config');
+
+    readyButton.addEventListener('click', startRoom);
+
+    function startRoom() {
+        if(!title.value || !time.value || !minBetSelect.value || !maxBetSelect.value || !pokerRounds.value) {
+
+            const modal = document.getElementById("myModal");
+            const span = document.getElementsByClassName("close")[0];
+            
+            modal.style.display = "block";
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        }
+
+        saveData(title.value, time.value, minBetSelect.value, maxBetSelect.value, pokerRounds.value);
+        window.location.replace("blackjack-dealer.html");
+    }
+
+    function saveData(title, time, minBet, maxBet, rounds) {
+        let match = Math.random().toString().substring(2);
+        const game = { title, time, minBet, maxBet, rounds }
+        localStorage.setItem(`game#${match}`, JSON.stringify(game));
+    }
